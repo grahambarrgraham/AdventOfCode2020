57b397e00b33f94ef9ca6f6c8eabbaca8ef4e4a9
gbarr <grahambarrgrahamn@gmail.com>
2020-12-03 08:52:17 +0000

    day3

--- src/main/kotlin/Day3.kt
+++ src/main/kotlin/Day3.kt
@@ -3,8 +3,41 @@
  */
 object Day3 {
 
-    fun part1(strings: List<String>): Int = 0
+    class Map(strings: List<String>) {
+        var map = strings;
+        var width = map[0].length
+        var currentX = 0;
+        var currentY = 0;
 
-    fun part2(strings: List<String>): Int = 0
+        fun isFinished() = currentY >= map.size
+        fun isTreeHere(): Boolean = map[currentY][currentX % width] == '#'
+        fun next(right: Int, down: Int) {
+            currentX += right
+            currentY += down
+        }
+    }
+
+    fun part1(strings: List<String>): Int {
+        return calc(strings, 3, 1)
+    }
+
+    fun part2(strings: List<String>): Int =
+            calc(strings, 1, 1) *
+                    calc(strings, 3, 1) *
+                    calc(strings, 5, 1) *
+                    calc(strings, 7, 1) *
+                    calc(strings, 1, 2)
+
+    fun calc(strings: List<String>, right: Int, down: Int): Int {
+        val map = Map(strings)
+        var count = 0;
+        while (!map.isFinished()) {
+            if (map.isTreeHere()) count++;
+            map.next(right, down)
+        }
+
+        return count;
+    }
+
 
 }
--- src/test/kotlin/Day3Test.kt
+++ src/test/kotlin/Day3Test.kt
@@ -7,22 +7,24 @@ class Day3Test {
 
     @Test
     fun part1_simple() {
-        assertThat(Day3.part1(loadStringList("/day3_simple.txt")), `is`(0))
+        assertThat(Day3.part1(loadStringList("/day3_simple.txt")), `is`(7))
+        assertThat(Day3.calc(loadStringList("/day3_simple.txt"), 1, 2), `is`(2))
+        assertThat(Day3.calc(loadStringList("/day3_simple.txt"), 7, 1), `is`(4))
     }
 
     @Test
     fun part1() {
-        assertThat(Day3.part1(loadStringList("/day3_data.txt")), `is`(0))
+        assertThat(Day3.part1(loadStringList("/day3_data.txt")), `is`(250))
     }
 
     @Test
     fun part2_simple() {
-        assertThat(Day3.part2(loadStringList("/day3_simple.txt")), `is`(0))
+        assertThat(Day3.part2(loadStringList("/day3_simple.txt")), `is`(336))
     }
 
     @Test
     fun part2() {
-        assertThat(Day3.part2(loadStringList("/day3_data.txt")), `is`(0))
+        assertThat(Day3.part2(loadStringList("/day3_data.txt")), `is`(1592662500))
     }
 
 }
