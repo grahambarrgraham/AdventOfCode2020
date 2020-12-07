1b86182f7fe928b1f5d691da1b86777b5aef9814
gbarr <grahambarrgrahamn@gmail.com>
2020-12-07 10:10:17 +0000

    tidy day7

--- src/main/kotlin/Day7.kt
+++ src/main/kotlin/Day7.kt
@@ -3,47 +3,39 @@
  */
 object Day7 {
 
-    fun part1(strings: List<String>): Int {
-        return "shiny gold".findRoots(load(strings)).size
-    }
+    fun part1(strings: List<String>): Int = "shiny gold".findRoots(load(strings)).size
 
-    fun part2(strings: List<String>): Int {
-        return "shiny gold".countBags(load(strings))
-    }
+    fun part2(strings: List<String>): Int = "shiny gold".countBags(load(strings))
 
     private fun String.countBags(parentMap: Map<String, List<Bag>>): Int {
         val count = parentMap[this]?.map { it.number }?.sum() ?: 0
-        val map = parentMap[this]?.map { it.colour.countBags(parentMap) * it.number}?.sum() ?: 0
-        val a = this
-        println("$a count : $count parentCount : $map")
-        return count + map
+        val parentCount = parentMap[this]?.map { it.bagType.countBags(parentMap) * it.number}?.sum() ?: 0
+        return count + parentCount
     }
 
     private fun String.findRoots(parentMap: Map<String, List<Bag>>): Set<String> {
-        val map = parentMap.filter { it.value.map { it.colour }.contains(this) }.keys
-        val parentSet = map.flatMap { it.findRoots(parentMap) }.toSet()
-        return map + parentSet
+        val directlyContained = parentMap.filter { it.value.map { bag -> bag.bagType }.contains(this) }.keys
+        val indirectlyContained = directlyContained.flatMap { it.findRoots(parentMap) }.toSet()
+        return directlyContained + indirectlyContained
     }
 
-    private fun load(strings: List<String>): Map<String, List<Bag>> {
-        return strings.map { it.parseTo() }.associate { it.first to it.second }
-    }
+    private fun load(strings: List<String>): Map<String, List<Bag>> = strings.map { it.parseLine() }.associate { it.first to it.second }
 
-    private val pattern = Regex("(.+) bags contain (.+)\\.")
+    private val linePattern = Regex("(.+) bags contain (.+)\\.")
 
-    private val childPattern = Regex("(\\d+) (.+) bag(s*)")
+    private val bagPattern = Regex("(\\d+) (.+) bag(s*)")
 
-    data class Bag(val colour: String, val number: Int)
+    data class Bag(val bagType: String, val number: Int)
 
-    fun String.parseTo(): Pair<String, List<Bag>> {
-        val (parentS, childrenS) = pattern.find(this)!!.destructured
-        val children = childrenS.split(",").map { it.toBag() }.filterNotNull()
+    fun String.parseLine(): Pair<String, List<Bag>> {
+        val (parentS, childrenS) = linePattern.find(this)!!.destructured
+        val children = childrenS.split(",").map { it.parseBag() }.filterNotNull()
         return Pair(parentS.trim(), children)
     }
 
-    fun String.toBag(): Bag? {
+    fun String.parseBag(): Bag? {
         if ("no other bags" == this) return null
-        val (numS, name, _) = childPattern.find(this)!!.destructured
+        val (numS, name, _) = bagPattern.find(this)!!.destructured
         return Bag(name, numS.toInt())
     }
 
