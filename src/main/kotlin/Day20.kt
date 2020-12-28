4b780cbed41a1b521e28544761dc51e1ef39e480
gbarr <grahambarrgrahamn@gmail.com>
2020-12-28 13:41:39 +0000

    day20 part2 prep

--- src/main/kotlin/Day20.kt
+++ src/main/kotlin/Day20.kt
@@ -4,39 +4,72 @@
 object Day20 {
 
     fun part1(strings: List<List<String>>): Long {
+        return arrangeTiles(strings)!!.corners.multiply()
+    }
+
+    fun part2(strings: List<List<String>>): Long {
+        val arrangement = arrangeTiles(strings)!!
+        val images : Map<String, List<List<String>>> = loadImages(strings)
+        val image : List<List<String>> = arrangeImage(arrangement, images)
+        val seahorses : Int = countSeaHorses(image)
+        val hashes = image.flatten().map { it.count { it == '#' } }.sum()
+        return (hashes - (15 * seahorses)).toLong()
+    }
+
+    private fun countSeaHorses(image: List<List<String>>): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    private fun arrangeImage(arrangement: Tile, images: Map<String, List<List<String>>>): List<List<String>> {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    private fun loadImages(strings: List<List<String>>): Map<String, List<List<String>>> {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    private fun arrangeTiles(strings: List<List<String>>): Tile? {
         val startingSet = readTiles(strings).toSet()
-        var t1: Set<Tile> = startingSet
+        var t1: List<Tile> = startingSet.toList()
         var result: Tile? = null
 
-        val done : MutableSet<Set<Tile>> = mutableSetOf()
+        //this is a crutch to stop pairs of tiles that have already been compared, being compared again
+        val done: MutableSet<Set<Tile>> = mutableSetOf()
 
         var counter = 1
-        while(result == null) {
+        while (result == null) {
             val t2 = combineTiles(t1, done)
-            t1 = t1 + dedupe(t2)
-            t1 = t1.filter { it.ids.size >= counter }.toSet()
+            t1 = dedupe(t2) + t1
+            t1 = t1.filter { it.ids.size >= counter }
+            //this is heuristic (hack) to aggressively exclude smaller tiles on subsequent iterations
+            //this reduces the search space as the total number of tiles increases on each iteration
+            //this is hacky way of implementing prioritisation to larger tiles pairs
             if (counter < 3 || startingSet.size < 32) counter++ else counter += 9
             result = t1.find { it.ids.size == startingSet.size }
         }
-
-        return result.corners.multiply()
+        return result
     }
 
-    private fun combineTiles(tiles: Set<Tile>, done: MutableSet<Set<Tile>>): Set<Tile> {
-        val foldIndexed = tiles.foldIndexed<Tile, Set<Tile>>(emptySet()) { index, acc, tile ->
+    private fun combineTiles(tiles: List<Tile>, done: MutableSet<Set<Tile>>): List<Tile> {
+
+        //this generates the full set of valid combined tiles from all combinations of the input tiles
+        //It is entirely brute force, it doesn't apply prioritisation of tile pairs which are good
+        //candidates nor does it complete eagerly when the full solution is found
+
+        return tiles.foldIndexed(emptyList()) { index, acc, tile ->
             acc + tiles
                     .drop(index + 1)
                     .filterNot { done.contains(setOf(tile, it)) }
                     .onEach { done.add(setOf(tile, it)) }
-                    .flatMap { tile.validPairs(it) }
+                    .filter { tile.isValidPair(it) }
+                    .map { tile.allValidPairs(it) }
+                    .flatMap { dedupe(it) }
         }
-        return foldIndexed
     }
 
     private fun readTiles(strings: List<List<String>>): List<Tile> = strings.map { readTile(it) }
 
     private fun readTile(strings: List<String>): Tile {
-        //println("Reading $strings")
         val (id) = "Tile (.+):".toRegex().find(strings[0])!!.destructured
         val tileS = strings.drop(1)
         val top = tileS.first()
@@ -46,11 +79,9 @@ object Day20 {
         return Tile(top, left, right, bottom, Corners(id, id, id, id), listOf(id))
     }
 
-    fun part2(strings: List<List<String>>): Long = 0
-
     data class Corners(val topLeft: String, val topRight: String, val bottomRight: String, val bottomLeft: String) {
 
-        val sortedHash = listOf(topLeft, topRight, bottomLeft, bottomRight).sorted().fold("") {acc, s -> acc + s}
+        val sortedHash = listOf(topLeft, topRight, bottomLeft, bottomRight).sorted().fold("") { acc, s -> acc + s }
 
         fun multiply(): Long = topLeft.toLong() * topRight.toLong() * bottomLeft.toLong() * bottomRight.toLong()
 
@@ -59,9 +90,13 @@ object Day20 {
         fun flipHorizontal() = Corners(topRight, topLeft, bottomLeft, bottomRight)
     }
 
-    data class Tile(val top: String, val left: String, val right: String, val bottom: String, val corners: Corners, val ids: List<String>) {
+    enum class Op {Rotate, FlipVertical, FlipHorizontal}
 
-        val sideLengths = setOf(left.length, right.length, top.length, right.length)
+    data class Tile(val top: String, val left: String, val right: String, val bottom:
+        String, val corners: Corners, val ids: List<String>, val parents: Parents? = null,
+                    val ops : List<Op> = emptyList()) {
+
+        private val sideLengths = setOf(left.length, right.length, top.length, right.length)
 
         private var cache: Set<Tile>? = null
 
@@ -69,52 +104,34 @@ object Day20 {
             cache = findAllValidTransformations(); cache!!
         } else cache!!
 
-        private fun flipVertical() = Tile(bottom, left.reversed(), right.reversed(), top, corners.flipVertical(), ids)
-        private fun flipHorizontal() = Tile(top.reversed(), right, left, bottom.reversed(), corners.flipHorizontal(), ids)
-        private fun r1() = Tile(left.reversed(), bottom, top, right.reversed(), corners.r1(), ids)
+        private fun flipVertical() = Tile(bottom, left.reversed(), right.reversed(), top, corners.flipVertical(), ids, parents, ops + Op.FlipVertical)
+        private fun flipHorizontal() = Tile(top.reversed(), right, left, bottom.reversed(), corners.flipHorizontal(), ids, parents, ops + Op.FlipHorizontal)
+        private fun r1() = Tile(left.reversed(), bottom, top, right.reversed(), corners.r1(), ids, parents, ops + Op.Rotate)
 
         private fun findAllValidTransformations(): Set<Tile> = rotations(flipHorizontal()) + rotations(flipVertical()) + rotations(this)
 
         private fun rotations(tile: Tile): Set<Tile> = setOf(tile, tile.r1())
 
-        fun validPairs(t: Tile): Set<Tile> = dedupe(allValidPairs(t))
+        fun isValidPair(t: Tile): Boolean =
+                (sideLengths.intersect(t.sideLengths).isNotEmpty()) && (ids.intersect(t.ids).isEmpty())
 
-        fun allValidPairs(t: Tile): Set<Tile> {
-
-            if (sideLengths.intersect(t.sideLengths).isEmpty()) {
-                return emptySet()
-            }
-
-            if (ids.intersect(t.ids).isNotEmpty()) {
-                return emptySet()
-            }
-
-            val toSet = this.transformations().flatMap { it1 ->
-                t.transformations().flatMap { it2 ->
-                    listOfNotNull(
-                            topBottom(it1, it2),
-                            topBottom(it2, it1),
-                            leftRight(it1, it2),
-                            leftRight(it2, it1))
+        //this performs more comparisons that is necessary (a max of 32 are required), so a dedupe is required
+        fun allValidPairs(t: Tile): List<Tile> =
+                transformations().flatMap { it1 ->
+                    t.transformations().mapNotNull { it2 -> topBottom(it2, it1) }
                 }
-            }.toSet()
-
-            return toSet
-        }
 
         private fun topBottom(t1: Tile, t2: Tile): Tile? =
                 if (t1.bottom == t2.top) Tile(t1.top, t1.left + t2.left, t1.right + t2.right, t2.bottom,
                         Corners(t1.corners.topLeft, t1.corners.topRight, t2.corners.bottomRight, t2.corners.bottomLeft),
-                        t1.ids + t2.ids) else null
-
-        private fun leftRight(t1: Tile, t2: Tile): Tile? =
-                if (t1.right == t2.left) Tile(t1.top + t2.top, t1.left, t2.right, t1.bottom + t2.bottom,
-                        Corners(t1.corners.topLeft, t2.corners.topRight, t2.corners.bottomRight, t1.corners.bottomLeft),
-                        t1.ids + t2.ids) else null
+                        t1.ids + t2.ids, Parents(t1, t2, true)) else null
     }
 
-    fun dedupe(s: Set<Tile>) : Set<Tile> {
-        return s.groupBy { it.corners.sortedHash }.map { it.value.first() }.toSet()
+    //this is a crutch (hack) to remove duplicate tile pairs that are produced unnecessarily
+    fun dedupe(s: List<Tile>): List<Tile> {
+        return s.groupBy { it.corners.sortedHash }.map { it.value.first() }
     }
+
+    data class Parents(val t1: Tile, val t2: Tile, val isTopBotton: Boolean)
 
 }
